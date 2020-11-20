cfda21ca7753d7b6989a7b8d35cfe5b6b987a23a
Ubuntu <ubuntu@ip-172-31-84-246.ec2.internal>
2020-11-20 20:28:34 +0000

    added in helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Wenda Zhao All rights reserved
+//
+// Created by Wenda Zhao
+// Created on Nov 2020
+// This is a "Hello world" program with proper style
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World! 你好，世界！" << std::endl;
+}
